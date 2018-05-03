5f976a539a6cc5c028cc5e74bc2803468b073dd6
310282744 <manit.kant@philips.com>
2018-05-03 18:32:57 +0530

    count implementation and error handling

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -14,11 +14,32 @@ export class CardComponent implements OnInit {
   cards: Data[];
   public modalRef: BsModalRef;
   favorite: boolean = true;
+  favoriteCount: number = 0;
+  public favoritesArray = [];
+  public nonFavoriteArray = [];
 
   constructor(private fetchData: FetchDataService, private modalService: BsModalService, private router: Router) { }
 
   ngOnInit() {
-    this.fetchData.getData().subscribe( data => {this.cards = [...data];})
+    this.fetchData.getData().subscribe( data => {
+      this.cards = [...data];
+      if(this.cards.length){
+        for(let cardFavorite of this.cards) {
+                if(cardFavorite.isFavourite) {
+                  this.favoritesArray.push(cardFavorite);
+                  this.favoriteCount++;
+
+                } else {
+                  this.nonFavoriteArray.push(cardFavorite);
+                }
+        }
+
+      } else {
+        console.log('No data returned');
+      }
+
+  },
+  error=> console.log('error msg', error))
   }
 
   public openModal(template: TemplateRef<any>) {
@@ -31,6 +52,28 @@ export class CardComponent implements OnInit {
 
   public openCard(_id: string) {
       this.router.navigate(['/detail', _id]);
+  }
+
+  public markFavorite(data: Data){
+
+      for(let cardDetail of this.cards){
+        if(data._id === cardDetail._id){
+          if(data.isFavourite){
+            cardDetail.isFavourite = false;
+            this.favoriteCount--;
+            this.favoritesArray.splice(this.favoritesArray.findIndex(card=> card._id === data._id), 1);
+            this.nonFavoriteArray.push(cardDetail);
+          } else {
+            cardDetail.isFavourite = true;
+            this.favoriteCount++;
+            this.favoritesArray.push(cardDetail);
+            this.nonFavoriteArray.splice(this.nonFavoriteArray.findIndex(card=> card._id === data._id), 1);
+          }
+
+        }
+      }
+
+
   }
 
 }
